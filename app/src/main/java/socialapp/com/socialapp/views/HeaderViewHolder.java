3a3c5f1fbc2a3e9d7f1a17362b0214d6a578b36a
Samar Ali <samarali@live.com>
2016-04-24 13:39:38 +0500

    part 09 - video 04

---
+++ app/src/main/java/socialapp/com/socialapp/views/HeaderViewHolder.java
@@ -0,0 +1,25 @@
+package socialapp.com.socialapp.views;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by SAMAR on 4/24/2016.
+ */
+public class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+
+    private final TextView textView;
+
+    public HeaderViewHolder(LayoutInflater inflater, ViewGroup parent) {
+        super(inflater.inflate(android.R.layout.simple_list_item_1, parent, false));
+        textView = (TextView) itemView.findViewById(android.R.id.text1);
+    }
+
+    public void populate(String text) {
+        textView.setText(text);
+    }
+
+}
